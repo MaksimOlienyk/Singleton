9eed7fdd365b7382bed29a9e00200347a0d2f4af
maljaethereal <121117752+MaksimOlienyk@users.noreply.github.com>
2025-10-19 18:12:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿class Singleton
+{
+    private static Singleton _instance;
+    public static Singleton Instance => _instance ??= new Singleton();
+    private Singleton() { }
+    public void Show() => Console.WriteLine("Singleton works");
+}
+
+class Program { static void Main() => Singleton.Instance.Show(); }
